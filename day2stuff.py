07f0d6f4c85bb079dd3d4eff2fc9b63de54dd5b0
kavanwills <meu5cg@virginia.edu>
2026-01-15 19:53:23 +0000

    ,

---
+++ day2stuff.py
@@ -0,0 +1,14 @@
+#%%
+# /workspaces/daytwostuff
+#%%
+# No, because you can use a requirements.txt file to give future users the requirements
+# The display path is not different.
+#%%
+import pandas as pd
+x = pd.read_csv("penguins.csv")
+#%%
+# There is now a local section and a codespace section in the extension menu
+# Visualize & filter large tabular datasets, one-click transforms (fill, drop, type-cast…), Automatic Pandas code preview & export
+#%%
+# We use a requirements.txt file to allow fututre users to access the necessary packages and versions.
+#%%
