724150646604a657ceaaa7afb4728664d61b0fd6
kavanwills <meu5cg@virginia.edu>
2026-01-15 20:34:19 +0000

    ,

--- day2stuff.py
+++ day2stuff.py
@@ -1,8 +1,8 @@
 #%%
-# /workspaces/daytwostuff
+# @kavanwills ➜ /workspaces/daytwostuff (main) $
 #%%
 # No, because you can use a requirements.txt file to give future users the requirements
-# The display path is not different.
+# (.venv) @kavanwills ➜ /workspaces/daytwostuff (main) $ , so it now says (.venv) indicating an active virtual environment
 #%%
 import pandas as pd
 x = pd.read_csv("penguins.csv")
@@ -10,7 +10,7 @@ x = pd.read_csv("penguins.csv")
 # There is now a local section and a codespace section in the extension menu
 # Visualize & filter large tabular datasets, one-click transforms (fill, drop, type-cast…), Automatic Pandas code preview & export
 #%%
-# We use a requirements.txt file to allow future users to access the necessary packages and versions.
+# We use a requirements.txt file to allow fututre users to access the necessary packages and versions.
 #%%
 # Recipe: Install necessary extensions in VS Code (Python, Data Wrangler, Jupyter, etc.), create a file (if .py add ipykernel for interactive), activate virtual environment in terminal, then make a requirements.txt file with any packages/versions you need
 # %%
